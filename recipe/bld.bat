33993eae15f8fa5b653eee85c97a5c7f485b8e68
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2024-07-29 16:25:44 +0100

    customize for local Windows builds using MSVC + Intel Fortran

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,18 +1,17 @@
 
 (
 echo [build_ext]
-echo cmake_opts=-DPython3_EXECUTABLE="%PYTHON%" -DCMAKE_C_COMPILER=gcc -G "MinGW Makefiles"
+echo cmake_opts=-DPython3_EXECUTABLE="%PYTHON%"
 echo compiler=gfortran
 ) > "%SRC_DIR%\python\setup.cfg"
 
-set CFLAGS=-DMS_WIN64
 set CMAKE_BUILD_PARALLEL_LEVEL=%CPU_COUNT%
 
 python -m pip install --no-deps -v "%SRC_DIR%\python"
 if errorlevel 1 exit /b 1
 
 set BUILD_DIR=%SRC_DIR%\extern\pygsw\build
-cmake -S "%SRC_DIR%\extern\pygsw" -B "%BUILD_DIR%" -DCMAKE_BUILD_TYPE=Release -DPython3_EXECUTABLE="%PYTHON%" -DCMAKE_Fortran_COMPILER=gfortran -DCMAKE_C_COMPILER=gcc -G "MinGW Makefiles"
+cmake -S "%SRC_DIR%\extern\pygsw" -B "%BUILD_DIR%" -DCMAKE_BUILD_TYPE=Release -DPython3_EXECUTABLE="%PYTHON%"
 if errorlevel 1 exit /b 1
 cmake --build "%BUILD_DIR%" --target pygsw_wheel --config Release --parallel %CPU_COUNT%
 if errorlevel 1 exit /b 1
@@ -20,7 +19,7 @@ xcopy /E /I "%BUILD_DIR%\pygsw" "%SP_DIR%\pygetm\pygsw"
 if errorlevel 1 exit /b 1
 
 set BUILD_DIR=%SRC_DIR%\extern\python-otps2\build
-cmake -S "%SRC_DIR%\extern\python-otps2" -B "%BUILD_DIR%" -DCMAKE_BUILD_TYPE=Release -DPython3_EXECUTABLE="%PYTHON%" -DCMAKE_Fortran_COMPILER=gfortran -DCMAKE_C_COMPILER=gcc -G "MinGW Makefiles"
+cmake -S "%SRC_DIR%\extern\python-otps2" -B "%BUILD_DIR%" -DCMAKE_BUILD_TYPE=Release -DPython3_EXECUTABLE="%PYTHON%"
 if errorlevel 1 exit /b 1
 cmake --build "%BUILD_DIR%" --target otps2_wheel --config Release --parallel %CPU_COUNT%
 if errorlevel 1 exit /b 1
